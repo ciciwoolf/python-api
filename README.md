46be780968b1e551e536404fa6059c6c079e844b
Cici Woolf <christinemarie.woolf@gmail.com>
2024-04-14 17:21:01 -0500

    Update readme

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # python-api
+
+App using Python, Flask, Marshmallow to boostrap an api
+https://auth0.com/blog/developing-restful-apis-with-python-and-flask/#-span-id--bootstrapping-flask----span--Bootstrapping-a-Flask-Application
+
+# start the cashman application
+
+./bootstrap.sh &
+
+# get incomes
+
+curl http://localhost:5000/incomes
+
+# add new income
+
+curl -X POST -H "Content-Type: application/json" -d '{
+"description": "band gig",
+"amount": 1000.0
+}' http://localhost:5000/incomes
--- cashman/index.py
+++ cashman/index.py
@@ -1,18 +1,48 @@
 from flask import Flask, jsonify, request
 
+from cashman.model.expense import Expense, ExpenseSchema
+from cashman.model.income import Income, IncomeSchema
+from cashman.model.transaction_type import TransactionType
+
 app = Flask(__name__)
 
-incomes = [
-    { 'description': 'salary', 'amount': 5000 }
+transactions = [
+    Income('Salary', 5000),
+    Income('Dividends', 200),
+    Expense('Dark Chocolate Truffles', 50),
+    Expense('Muay Thai gear', 100)
 ]
-
 
 @app.route('/incomes')
 def get_incomes():
+    schema = IncomeSchema(many=True)
+    incomes = schema.dump(
+        filter(lambda t: t.type == TransactionType.INCOME, transactions)
+    )
     return jsonify(incomes)
-
 
 @app.route('/incomes', methods=['POST'])
 def add_income():
-    incomes.append(request.get_json())
-    return '', 204
+    income = IncomeSchema().load(request.get_json())
+    transactions.append(income)
+    return "", 204
+
+@app.route('/expenses')
+def get_expenses():
+    schema = ExpenseSchema(many=True)
+    expenses = schema.dump(
+        filter(lambda t: t.type == TransactionType.EXPENSE, transactions)
+    )
+    return jsonify(expenses)
+
+@app.route('/expenses', methods=['POST'])
+def add_expense():
+    expense = ExpenseSchema().load(request.get_json())
+    transactions.append(expense)
+    return "", 204
+
+
+if __name__ == "__main__":
+    app.run()
+
+    
